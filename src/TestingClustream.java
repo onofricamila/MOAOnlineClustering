215b8b1ab02ccb376150d73ccd956524c87b13c4
Camila Onofri <onofricamila2@gmail.com>
2019-11-20 16:18:31 -0300

    clu stream params tuneados

--- src/TestingClustream.java
+++ src/TestingClustream.java
@@ -1,20 +1,35 @@
+import com.yahoo.labs.samoa.instances.DenseInstance;
 import moa.cluster.Cluster;
 import moa.cluster.Clustering;
 import moa.cluster.SphereCluster;
 import moa.clusterers.clustream.WithKmeans;
-import static data.InstanceGenerator.randomInstance;
+import moa.core.InstanceExample;
+import moa.streams.clustering.SimpleCSVStream;
+
+import static data.StreamFromCsvGenerator.simpleCSVStream;
 
 public class TestingClustream {
     public static void main(String[] args) {
         WithKmeans wkm = new WithKmeans();
-        wkm.kOption.setValue(5);
+        wkm.kOption.setValue(2);
         wkm.timeWindowOption.setValue(100);
         wkm.maxNumKernelsOption.setValue(1000);
-        wkm.kernelRadiFactorOption.setValue(5);
+    //    wkm.kernelRadiFactorOption.setValue(5);
         wkm.resetLearningImpl();
 
-        for (int i = 0; i < 10000; i++) {
+        /*for (int i = 0; i < 10000; i++) {
             wkm.trainOnInstanceImpl(randomInstance(2));
+        }
+*/
+        SimpleCSVStream stream = simpleCSVStream();
+        while (stream.hasMoreInstances()) {
+            InstanceExample trainInst = stream.nextInstance();
+            // get dense instance
+            DenseInstance inst = (DenseInstance) trainInst.instance;
+            // by empty class
+            inst.deleteAttributeAt(2);
+            //learning code
+            wkm.trainOnInstanceImpl(inst);
         }
 
         Clustering clusteringResult = wkm.getClusteringResult();
