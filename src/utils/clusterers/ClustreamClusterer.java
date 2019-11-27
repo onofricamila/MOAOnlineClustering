a154c35009814f3501d082aae7fddb441e0f1f52
Camila Onofri <onofricamila2@gmail.com>
2019-11-27 13:45:20 -0300

    documentation added to clusterers regarding their parameters

--- src/utils/clusterers/ClustreamClusterer.java
+++ src/utils/clusterers/ClustreamClusterer.java
@@ -38,11 +38,22 @@ public class ClustreamClusterer extends BasicClusterer {
     @Override
     public AbstractClusterer prepareClusterer(int initMinPoints) {
         WithKmeans withKmeans = new WithKmeans();
-        withKmeans.kOption.setValue(2);
-        withKmeans.timeWindowOption.setValue(1000); // default
+        /* horizon: Defines the time window to be used in CluStream (snapshots horizon). */
+        withKmeans.timeWindowOption.setValue(1000); // = default 1000
+
+        /* m: Defines the maximum number of micro-clusters used in CluStream. A buffer of the same size will be used
+        for initialization: once the buffer is full, the macro clusters start being formed. Default: 100. */
         // TODO: vary maxNumKernelsOption. A buffer of the same size will be used for initialization
         withKmeans.maxNumKernelsOption.setValue(initMinPoints-1);
-        //    wkm.kernelRadiFactorOption.setValue(5);
+
+        /* t: Maximal boundary factor (=Kernel radius factor). When deciding to add a newdata point to a micro-cluster,
+        the maximum boundary is defined as a factor oftof the RMS deviation of the data points in the micro-cluster from the centroid. */
+        withKmeans.kernelRadiFactorOption.setValue(2); // = default 2
+
+        /* k: Number of macro-clusters to produce using weighted k-means. Default: 5. */
+        withKmeans.kOption.setValue(2);
+
+        // once the parameters are specified, prepare the clusterer
         withKmeans.resetLearningImpl();
         return withKmeans;
     }
--- src/utils/clusterers/DenstreamClusterer.java
+++ src/utils/clusterers/DenstreamClusterer.java
@@ -39,13 +39,34 @@ public class DenstreamClusterer extends BasicClusterer {
     @Override
     public AbstractClusterer prepareClusterer(int initMinPoints) {
         WithDBSCAN withDBSCAN = new WithDBSCAN();
-        // no matter the horizonOption, the result you get is the same
-        withDBSCAN.horizonOption.setValue(1000); // default
-        withDBSCAN.epsilonOption.setValue(0.4); // TODO: vary
+        /* horizon: ?. No matter the value, the result you get is the same. */
+        withDBSCAN.horizonOption.setValue(1000); // = default 1000
+
+        /* epsilon: defines the epsilon neighbourhood which is the maximal radius of micro-clusters(r<=epsilon). Range [0, 1] */
+        // TODO: vary epsilonOption,
+        withDBSCAN.epsilonOption.setValue(0.4); // default 0.2
+
+        /* beta: multiplier for mu to detect outlier micro-clusters given their weight w (w<betax mu). Range [0, 1] */
+        withDBSCAN.betaOption.setValue(0.2); // = default 0.2
+
+        /* mu: minpoints as the weight w a core-micro-clusters needs to be created (w>=mu). Range [0, +inf] */
+        withDBSCAN.muOption.setValue(1); // = default 1
+
+        /* initPoints: number of points to use for initialization via DBSCAN. Default: 1000  */
         // TODO: vary initPointsOption (needed amount of points to start forming macro clusters)
         withDBSCAN.initPointsOption.setValue(initMinPoints);
-        // role: set timestamp value (does not mean batches to process)
-        withDBSCAN.speedOption.setValue(100); // default
+
+        /* offline: offline multiplier for epsilon. Used for reachabilityreclustering. Range [2, 20] */
+        withDBSCAN.offlineOption.setValue(2); // = default 2
+
+        /* lambda: decay constant. Range [0.25, 1] */
+        // TODO: vary lambda (forgetting component)
+        withDBSCAN.lambdaOption.setValue( Double.parseDouble("".trim()) ); // = default ""
+
+        /* processingSpeed: number of incoming points per time unit (important for decay). Role: set timestamp value (does not mean "batches to process") */
+        withDBSCAN.speedOption.setValue(100); // = default 100
+
+        // once the parameters are specified, prepare the clusterer
         withDBSCAN.resetLearningImpl();
         withDBSCAN.initialDBScan();
         return withDBSCAN;
