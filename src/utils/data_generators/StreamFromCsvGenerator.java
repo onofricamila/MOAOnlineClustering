cd2e1e130efe73f2e91992102ad3b57c808f9584
Camila Onofri <onofricamila2@gmail.com>
2019-12-07 13:55:58 -0300

    clustering results se guardan en file name without extention

--- src/utils/data_generators/StreamFromCsvGenerator.java
+++ src/utils/data_generators/StreamFromCsvGenerator.java
@@ -8,7 +8,7 @@ import static config.Config.getTimeSeriesToyDatasetName;
 public class StreamFromCsvGenerator {
     public static SimpleCSVStream simpleCSVStream() {
         SimpleCSVStream stream = new SimpleCSVStream();
-        String pathToCSV = getTimeSeriesDatasetsPath() + getTimeSeriesToyDatasetName();
+        String pathToCSV = getTimeSeriesDatasetsPath() + getTimeSeriesToyDatasetName() + ".csv";
         stream.csvFileOption.setValue(pathToCSV);
         stream.classIndexOption.setValue(false); //last column is class lable
         stream.prepareForUse();
